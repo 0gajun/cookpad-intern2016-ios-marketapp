5b2a16b6b3beef490e8b9b29252b32f6b604ba53
Junya Ogasawara <junya-ogasawara@cookpad.jp>
2016-08-16 12:05:06 +0900

    Create simple recommend item list(in Lesson04)

--- Market/View/RecommendItemCell.swift
+++ Market/View/RecommendItemCell.swift
@@ -13,4 +13,10 @@ class RecommendItemCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func update(withItem item: Item) {
+        nameLabel.text = item.name
+        priceLabel.text = "\(item.price)円"
+        descriptionLabel.text = item.desc
+    }
 }
