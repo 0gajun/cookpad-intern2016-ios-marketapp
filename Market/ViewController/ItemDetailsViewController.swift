44e1ce0441d353b1ec46343426089e20c9922035
Junya Ogasawara <junya-ogasawara@cookpad.jp>
2016-08-16 15:18:13 +0900

    Show item detail in ItemDetailsViewController.

--- Market/ViewController/ItemDetailsViewController.swift
+++ Market/ViewController/ItemDetailsViewController.swift
@@ -7,24 +7,43 @@
 //
 
 import UIKit
+import APIKit
+import WebImage
 
 class ItemDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var addCartButton: UIButton!
     
     var itemID: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "読込中..."
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        print(itemID)
+        let request = ItemRequest(id: itemID)
+        Session.sendRequest(request) { result in
+            switch result {
+            case .Success(let response):
+                self.updateView(response)
+            case .Failure(let error):
+                print(error)
+            }
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateView(item: Item) {
+        thumbnailImage.sd_setImageWithURL(item.imageURL)
+        nameLabel.text = item.name
+        descriptionLabel.text = item.desc
+        priceLabel.text = "\(item.price)円"
+        self.title = item.name
     }
 }
