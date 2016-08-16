3a8f8e7ff848d83a1f5a0719b612c50fbb646294
Junya Ogasawara <junya-ogasawara@cookpad.jp>
2016-08-16 11:08:43 +0900

    Add RecommendItemCell.

---
+++ Market/View/RecommendItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecommendItemCell.swift
+//  Market
+//
+//  Created by junya-ogasawara on 2016/08/16.
+//  Copyright © 2016年 COOKPAD inc,. All rights reserved.
+//
+
+import UIKit
+
+class RecommendItemCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
